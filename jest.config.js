d87b5657e53d4cba6236bbaf612d3683c251e2ea
Daniel Chege Njenga <danychege28@gmail.com>
2025-07-17 12:18:22 +0300

    Add files via upload

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,38 @@
 // jest.config.js - Root Jest configuration file
 
 module.exports = {
-  // Base configuration for all tests
+  verbose: true,
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      branches: 60,
+      functions: 70,
+      lines: 70,
+    },
+  },
+  testTimeout: 10000,
   projects: [
     // Server-side tests configuration
     {
       displayName: 'server',
+      setupFiles: ['<rootDir>/server/tests/integration/jest.setup.js'],
+      testMatch: ['**/server/tests/unit/**/*.test.js', '**/server/tests/integration/**/*.test.js'],
       testEnvironment: 'node',
-      testMatch: ['<rootDir>/server/tests/**/*.test.js'],
-      moduleFileExtensions: ['js', 'json', 'node'],
-      setupFilesAfterEnv: ['<rootDir>/server/tests/setup.js'],
+      moduleDirectories: ['node_modules', 'server/src'],
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/server/src/$1',
+      },
+      transform: {
+        '^.+\\.js$': ['babel-jest', { presets: ['@babel/preset-env'] }],
+      },
+      transformIgnorePatterns: [
+        '/node_modules/(?!(@babel|mongodb|mongoose|mongodb-memory-server)/)',
+      ],
+      globals: {
+        'NODE_ENV': 'test'
+      },
       coverageDirectory: '<rootDir>/coverage/server',
       collectCoverageFrom: [
         'server/src/**/*.js',
@@ -21,16 +44,16 @@ module.exports = {
     // Client-side tests configuration
     {
       displayName: 'client',
+      testMatch: ['**/client/src/tests/**/*.test.js'],
       testEnvironment: 'jsdom',
-      testMatch: ['<rootDir>/client/src/**/*.test.{js,jsx}'],
-      moduleFileExtensions: ['js', 'jsx', 'json'],
-      moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/client/src/tests/__mocks__/fileMock.js',
-      },
       setupFilesAfterEnv: ['<rootDir>/client/src/tests/setup.js'],
       transform: {
         '^.+\\.(js|jsx)$': 'babel-jest',
+      },
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/client/src/$1',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/client/src/tests/__mocks__/fileMock.js',
       },
       coverageDirectory: '<rootDir>/coverage/client',
       collectCoverageFrom: [
